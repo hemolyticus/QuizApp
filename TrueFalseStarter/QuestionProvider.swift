c4aa583f431a1c5b2a5409efbfa306f1916e6cc5
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-06-07 08:33:44 +1200

    Created a function that will remove a question from questionArray

--- TrueFalseStarter/QuestionProvider.swift
+++ TrueFalseStarter/QuestionProvider.swift
@@ -18,23 +18,28 @@ struct Questions {
     var correctAnswer: String
 }
 
-let question1 = Questions(question: "This was the only US President to serve more than two consecutive terms", option1: "George Washington", option2: "Franklin D. Roosevelt", option3: "Woodrow Wilson", option4: "Andrew Jackson", correctAnswer: "Franklin D. Roosevelt")
+var question1 = Questions(question: "This was the only US President to serve more than two consecutive terms", option1: "George Washington", option2: "Franklin D. Roosevelt", option3: "Woodrow Wilson", option4: "Andrew Jackson", correctAnswer: "Franklin D. Roosevelt")
 
-let question2 = Questions(question: "Which of the following countries has the most residents?", option1: "Nigeria", option2: "Russia", option3: "Iran", option4: "Vietnam", correctAnswer: "Nigeria")
+var question2 = Questions(question: "Which of the following countries has the most residents?", option1: "Nigeria", option2: "Russia", option3: "Iran", option4: "Vietnam", correctAnswer: "Nigeria")
 
-let question3 = Questions(question: "1918", option1: "1919", option2: "1919", option3: "1945", option4: "1954", correctAnswer: "1945")
+var question3 = Questions(question: "1918", option1: "1919", option2: "1919", option3: "1945", option4: "1954", correctAnswer: "1945")
 
-let question4 = Questions(question: "The Titanic departed from the United Kingdom, where was it supposed to arrive?", option1: "Paris", option2: "Washington D.C.", option3: "New York City", option4: "Boston", correctAnswer: "New York City")
+var question4 = Questions(question: "The Titanic departed from the United Kingdom, where was it supposed to arrive?", option1: "Paris", option2: "Washington D.C.", option3: "New York City", option4: "Boston", correctAnswer: "New York City")
 
-let question5 = Questions(question: "Which nation produces the most oil?", option1: "Iran", option2: "Iraq", option3: "Brazil", option4: "Canada", correctAnswer: "Canada")
+var question5 = Questions(question: "Which nation produces the most oil?", option1: "Iran", option2: "Iraq", option3: "Brazil", option4: "Canada", correctAnswer: "Canada")
 
-let question6 = Questions(question: "Which country has most recently won consecutive World Cups in Soccer?", option1: "Italy", option2: "Brazil", option3: "Argentina", option4: "Spain", correctAnswer: "Brazil")
+var question6 = Questions(question: "Which country has most recently won consecutive World Cups in Soccer?", option1: "Italy", option2: "Brazil", option3: "Argentina", option4: "Spain", correctAnswer: "Brazil")
 
-let questionArray: [Questions] = [question1,question2, question3, question4, question5, question6]
+var questionArray: [Questions] = [question1,question2, question3, question4, question5, question6]
 
 func generateRandomQuestion() -> Questions
 {
     let indexOfQuestion = GKRandomSource.sharedRandom().nextInt(upperBound: questionArray.count)
     
     return questionArray[indexOfQuestion]
+}
+
+func removeGeneratedQuestion(at index: Int)
+{
+    questionArray.remove(at: index)
 }
